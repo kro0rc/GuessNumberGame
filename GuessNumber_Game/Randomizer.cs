cf085d8eaa0c2821785b6a20f1472884604ba8de
krot_rc <savinyhnv@gmail.com>
2021-01-12 17:22:35 +0200

    Testin that git is ok

---
+++ GuessNumber_Game/Randomizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessNumber_Game
+{
+    class Randomizer
+    {
+        public int GetRandomNumber()
+        {
+            Random random = new Random();
+            int randomNum = random.Next(0, 100);
+            return randomNum;
+        }
+    }
+}

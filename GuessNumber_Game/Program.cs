cb185ad1c84d73841147eb74bb06bd9792339756
krot_rc <savinyhnv@gmail.com>
2021-01-16 19:56:04 +0200

    Cleared project

--- GuessNumber_Game/Program.cs
+++ GuessNumber_Game/Program.cs
@@ -3,55 +3,11 @@
 namespace GuessNumber_Game
 {
     class Program
-    {
-        Randomizer randomizer = new Randomizer();
-        
+    {   
         static void Main(string[] args)
         {
-            Program program = new Program();
-            program.StartGame();
+
         }
 
-        public void StartGame()
-        {
-            GameProcess game = new GameProcess();
-
-            Console.WriteLine("Try guess the number :)\nNumber is in range from 0 to 100.");
-
-            int generatedNumber = randomizer.GetRandomNumber();
-
-            game.NewGame(generatedNumber);
-            ExitDialog(UserInput("Choose optinos below : \n1. Try again\n2. Exit game"));
-        }
-
-        public int UserInput(string message)
-        {
-            Console.WriteLine(message);
-            
-            if ((Int32.TryParse(Console.ReadLine(), out int result)) && result> 0 && result < 3)
-            {
-                return result;
-            }
-
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Clear();
-            Console.WriteLine("Wrong input!");
-            Console.ResetColor();
-
-            return UserInput(message);
-        }
-
-        public void ExitDialog(int decision)
-        {
-            if (decision == 1)
-            {
-                StartGame();
-            }
-
-            if (decision == 2)
-            {
-                Console.WriteLine("Good Bye!");
-            }
-        }
     }
 }
